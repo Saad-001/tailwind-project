2af3ef7aa2b3cbd8cfe0d55d2538e52470b3d849
Saad-001 <saadafnan21@gmail.com>
2021-06-10 23:45:17 +0600

    second commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+import { Disclosure } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import logo from '../../images/pixabay-logo.png';
 import logoText from '../../images/pixabay-text.png';
 
