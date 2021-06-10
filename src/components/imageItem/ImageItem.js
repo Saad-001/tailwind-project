580db861171863a760c66533bdfc1e4813498ce7
Saad-001 <saadafnan21@gmail.com>
2021-06-10 23:50:34 +0600

    third commit

--- src/components/imageItem/ImageItem.js
+++ src/components/imageItem/ImageItem.js
@@ -7,7 +7,7 @@ const ImageItem = ({image}) => {
         <div className="max-w-sm mt-4 rounded overflow-hidden shadow-lg">
         <img src={image.webformatURL}className="w-full" alt="" />
         <div className="px-6 py-4">
-          <div className="font-bold text-purple-500 text-xl mb-2">
+          <div className="font-bold text-green-500 text-xl mb-2">
               Photo by {image.user}
           </div>
           <ul>
